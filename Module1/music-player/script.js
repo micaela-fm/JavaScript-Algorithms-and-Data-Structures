9917fdb6889f16569347a0141d51cef2921c27d4
micaela-fm <91810255+micaela-fm@users.noreply.github.com>
2025-02-12 22:48:48 +0000

    added a feature to highlight the song that is currently playing

--- Module1/music-player/script.js
+++ Module1/music-player/script.js
@@ -98,6 +98,7 @@ const playSong = (id) => {
   userData.currentSong = song;
   playButton.classList.add("playing");
 
+  highlightCurrentSong();
   audio.play();
 };
 
@@ -130,6 +131,19 @@ const playPreviousSong = () => {
 };
 
 
+
+const highlightCurrentSong = () => {
+  const playlistSongElements = document.querySelectorAll(".playlist-song");
+  const songToHighlight = document.getElementById(
+    `song-${userData?.currentSong?.id}`
+  );
+
+  playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute("aria-current");
+  });
+
+  if (songToHighlight) songToHighlight.setAttribute("aria-current", "true");
+};
 
 const renderSongs = (array) => {
   const songsHTML = array
