066d4d087826e978b6f00fb3a8a3442cf769a252
micaela-fm <91810255+micaela-fm@users.noreply.github.com>
2025-02-13 12:26:07 +0000

    add play button accessible text

--- Module1/music-player/script.js
+++ Module1/music-player/script.js
@@ -100,6 +100,7 @@ const playSong = (id) => {
 
   highlightCurrentSong();
   setPlayerDisplay();
+  setPlayButtonAccessibleText();
   audio.play();
 };
 
@@ -130,6 +131,8 @@ const playPreviousSong = () => {
     playSong(previousSong.id);
    }
 };
+
+  
 
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
@@ -176,7 +179,14 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
+const setPlayButtonAccessibleText = () => {
+  const song = userData?.currentSong || userData?.songs[0];
 
+  playButton.setAttribute(
+    "aria-label",
+    song?.title ? `Play ${song.title}` : "Play"
+  );
+};
 
 const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
 
